c01474fed4de3890dc80823630b0081948640d6f
Cristian Bolivar <supercris56@gmail.com>
2022-05-29 17:54:40 -0500

    Incorporacion Navbar

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,58 @@
+import React from 'react';
+import './styles/NavBar.css';
+import logoNav from '../components/images/logo.png';
+import { Link } from "react-router-dom";
+
+function NavBar() {
+  return (
+    <>
+      <nav className="navbar navbar-expand-lg navbar-light bg-light">
+        <div className="container-fluid">
+          <Link to="/">
+            <img src={logoNav} width='50' alt='Logo navbar'/>
+          </Link>
+          <span class="navbar-text px-4 glitch">
+            Tienda PC Componentes
+          </span>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarNav"
+            aria-controls="navbarNav"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarNav">
+            <ul className="navbar-nav mx-auto">
+              <li className="nav-item header__link">
+                <Link className="nav-link" to='/'>
+                  Inicio
+                </Link>
+              </li>
+              <li className="nav-item header__link">
+                <Link className="nav-link" to='/'>
+                  Productos
+                </Link>
+              </li>
+              <li className="nav-item header__link">
+                <Link className="nav-link" to='/'>
+                  Nosotros
+                </Link>
+              </li>
+              <li className="nav-item header__link">
+                <Link className="nav-link" to='/'>
+                  Contacto
+                </Link>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+    </>
+  );
+}
+
+export default NavBar
